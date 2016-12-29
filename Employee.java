16f54574ae0004e9b584304c1fd7d7ffa7a15c38
shubhadharwadkar27 <shubhadharwadkar27@gmail.com>
2016-12-29 11:52:06 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,9 +1,9 @@
 public class Employee
 {
-public String name;
-public int id;
-public int ssid;
-public double salary;
+ String name;
+ int id;
+ int ssid;
+ double salary;
 Employee(String name, int id,int ssid, double salary)
 {
 this.name=name;
@@ -13,36 +13,7 @@ this.salary=salary;
 }
 void display()
 {
-System.out.println(name+"" +id+"" +ssid+"" +salary);
-}
-
-
-class Manager extends Employee
-{
-public String dept;
-Manager(String name, int id, int ssid, double salary, String dept)
-{
-super(name,id,ssid,salary);
-this.dept=dept;
-}
-void display()
-{
-System.out.println(name+"" +id+"" +ssid+"" +salary+"" +dept);
-}
-}
-
-class Engineer extends Employee
-{
-public String designation;
-Engineer(String name,int id,int ssid,double salary, String designation)
-{
-super(name,id,ssid,salary);
-this.designation=designation;
-}
-void display1()
-{
-System.out.println(name+"" +id+"" +ssid+"" +salary+"" +designation);
-}
+System.out.println(name+" " +id+" " +ssid+" " +salary);
 }
 
 public static void main(String[] args)
@@ -52,8 +23,37 @@ Manager m=new Manager("Shubha",40, 39,35000,"HR");
 m.display();
 }
 {
-Engineer e=new Engineer("Shruti",36,45,500000,"Senior Engg");
+Engineer e=new Engineer("Shruti",36,45,500000.00,"Senior Engg");
 e.display1();
 }
+}
+}
+
+
+class Manager extends Employee
+{
+ String dept;
+public Manager(String name, int id, int ssid, double salary, String dept)
+{
+super(name,id,ssid,salary);
+this.dept=dept;
+}
+void display()
+{
+System.out.println(name+" " +id+" " +ssid+" " +salary+" " +dept);
+}
+}
+
+class Engineer extends Employee
+{
+ String designation;
+Engineer(String name,int id,int ssid,double salary, String designation)
+{
+super(name,id,ssid,salary);
+this.designation=designation;
+}
+void display1()
+{
+System.out.println(name+" " +id+" " +ssid+" " +salary+" " +designation);
 }
 }
